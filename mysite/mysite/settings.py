54c68e72af334ee790841f7c7901078ca6709ace
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-05-29 23:17:45 +1200

    heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -153,3 +153,5 @@ STATICFILES_DIRS = (
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+import django_heroku
+django_heroku.settings(locals())
