cbae0e00b8afda86d063b5bdd7a22963b7b73b12
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-05-30 19:03:45 +1200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'main.context_processors.add_variable_to_context',
             ],
         },
     },
