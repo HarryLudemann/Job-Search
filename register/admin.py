d463344da65fe8303e999cca8998d62226022567
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-05-30 15:52:13 +1200

    Update admin.py

--- register/admin.py
+++ register/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
-from register.models import Occupation
+from register.models import Occupation, Themes
 
 # Register your models here.
 admin.site.register(Occupation)
+admin.site.register(Themes)
