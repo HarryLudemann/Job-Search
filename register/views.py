61418b86a06d9b4909a1c2540fc4ac2f590d85a5
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-05-30 17:45:38 +1200

    Password Theme Fixes

--- register/views.py
+++ register/views.py
@@ -149,3 +149,7 @@ def password_reset_request(request):
 def passworddone(request):
     return redirect("/", {"theme":CheckDarkTheme(request)})
 
+def passworddone(request):
+    passwordchangeform = PasswordChangeForm()
+    return render(request, "register/change-password.html",{"form":passwordchangeform, "theme":CheckDarkTheme(request)})
+
