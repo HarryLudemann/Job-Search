9508ba32b8a943501971cda7654567413582527b
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-05-30 19:02:05 +1200

    Create context_processors.py

---
+++ main/context_processors.py
@@ -0,0 +1,17 @@
+# Checks theme
+def CheckDarkTheme(response):
+    obj = Themes.objects.all()
+    if (obj.filter(userid=response.user.id).exists()):
+        obj = obj.filter(userid=response.user.id)
+        for item in obj:
+            if (item.theme == "dark"):
+                return 'dark'
+            else:
+                return 'light'
+    else:
+        return 'light'
+
+def add_variable_to_context(request):
+    return {
+        'theme': CheckDarkTheme(request)
+    }
