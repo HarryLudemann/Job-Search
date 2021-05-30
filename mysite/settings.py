399ce541842eeddb1aee84098a57cf83638aacfc
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-05-30 21:40:34 +1200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
-
+LOGIN_URL = "/login"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
