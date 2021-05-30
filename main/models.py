219b86691e356252b2c5b5f25390b7f1e1a5a828
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-05-30 13:42:11 +1200

    Updated Job Model

--- main/models.py
+++ main/models.py
@@ -9,6 +9,7 @@ class Jobs(models.Model):
     location = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
     description = models.TextField()
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -16,8 +17,9 @@ class Jobs(models.Model):
         return self.careertype
         return self.hours
         return self.description
+        return self.location
         return self.date
-        return self.enddate
+        return self.email
     
 
 class Locations(models.Model):
