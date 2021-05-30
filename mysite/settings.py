ec9513e35e087e419452083e4f7b000f933d211c
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-05-30 14:09:21 +1200

    Testing Server(500) fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-r9msy-5kyf3kg1=yj7w(z+1&70&3b51(@%va#vnnl1n%*!t6+d
 DEBUG = False
 
 ALLOWED_HOSTS = ['studentjobsearch.herokuapp.com']
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 
 # Application definition
